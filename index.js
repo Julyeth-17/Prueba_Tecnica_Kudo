3ceb53138bf97f4f519c655e652b4113bb125e9d
julieth <julieth.h27@gmail.com>
2025-06-17 11:38:22 -0500

    heroku deploy port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use('/api/user', require('./routes/user'));
 app.use('/api/movie', require('./routes/movie'))
 
-const port = process.env.port || 4000
-app.listen(port, () => {
-    console.log(`server up on port ${port}`)
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`server up on port ${PORT}`)
 })
